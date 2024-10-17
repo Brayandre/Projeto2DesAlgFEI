03a2967183ddd840782c2e7a73a3d6f517e93502
Brayandre <166436744+Brayandre@users.noreply.github.com>
2024-10-17 18:18:07 +0000

    criando carrinho

--- dec_global.h
+++ dec_global.h
@@ -7,6 +7,22 @@ struct Produtos{
     float preco;
 };
 
+struct Carrinho{
+    int id;
+    char data;
+    float valTot;
+}
+
+void pegarData(int *dia, int *mes){
+    
+    time_t d = time(NULL);
+
+    struct tm *dataA = localtime(&d);
+
+    *dia = dataA->tm_mday - 1;
+    *mes = dataA->tm_mon + 1;    
+}
+
 void f_produtos(struct Produtos produto[], int tamanho);
 
 #endif
--- funcoes_cli.c
+++ funcoes_cli.c
@@ -18,16 +18,34 @@ void f_produtos(struct Produtos produto[], int tamanho){
     printf("6 - Padaria\n");
     printf("\n");
 
+    int qtdP;
+    char buy;
     int setor;
+    float valorP;
+
     printf("Digite o numero do setor de compras que deseja acessar: \n");
     scanf(" %d", &setor);
     if (setor == 1){
         for (int i = 0; i < tamanho ; i++){
-            printf("Nome: %s", produto[i].nome);
-            printf("Quantidade: %d", produto[i].quantidade);
-            printf("Preço: R$ %.2f", produto[i].preco);
-            printf("\n");
-            printf("**************************************");
+            printf("Nome: %s\n", produto[i].nome);
+            printf("Quantidade: %d\n", produto[i].quantidade);
+            printf("Preço: R$ %.2f\n", produto[i].preco);
+            printf("**************************************\n");
+            printf("voçê deseja comprar %s ? (S/N)\n ", produto[i].nome);
+            scanf("%s", &buy);
+            if (toupper(buy) == 'S'){
+                printf("Digite a quantidade que deseja comprar\n");
+                scanf("%d", &qtdP);
+                if (qtdP <= produto[i].quantidade){
+                    valorP = qtdP * produto[i].preco;
+                    printf("Valor adicionado ao carrinho: %.2f\n", valorP);
+                }
+                else{
+                    printf("Não há quantidade suficiente!")
+                }
+            }
+            printf("**************************************\n");
         }
+
     }
 }
